4a101463022f1bf606cc25c710e6cd0f64656751
aitmatov2005 <aitmatov2005@gmail.com>
2021-11-15 22:30:19 +0600

    my second commit

---
+++ code.py
@@ -0,0 +1,65 @@
+import re
+
+# file_path = 'MOCK_DATA.txt'
+# email_file_path = 'emails.txt'
+# file_reader = open(file_path, mode='r', encoding='Latin-1')
+# emails_file = open(email_file_path, mode='w', encoding='Latin-1')
+# my_text_1 = file_reader.read()
+
+# searching = r'[\w+_-]+@[\w+_-]+.[\w.]+'
+# result_all = re.findall(searching, my_text_1)
+
+# for item in result_all:
+#     print(item)
+#     emails_file.write(item + '\n')
+
+# print(f'Total :  {str(len(result_all))}')
+
+
+# file_path = 'MOCK_DATA.txt'
+# colors_file_path = 'colors.txt'
+# file_reader = open(file_path, mode='r', encoding='Latin-1')
+# colors_file = open(colors_file_path, mode='w', encoding='Latin-1')
+# my_text_2 = file_reader.read()
+
+# searching = r'[\d 0-9 #]+[\w a-z]+[\d]'
+# result_all = re.findall(searching, my_text_2)
+
+# for item in result_all:
+#     print(item)
+#     colors_file.write(item + '\n')
+
+# print(f'Total :  {str(len(result_all))}')
+
+
+# file_path = 'MOCK_DATA.txt'
+# full_name_file_path = 'full_name.txt'
+# file_reader = open(file_path, mode='r', encoding='Latin-1')
+# full_name_file = open(full_name_file_path, mode='w', encoding='Latin-1')
+# my_text_3 = file_reader.read()
+
+# searching = r'[A-Z][a-z]+[\s |]+\w+\w+[\w A-Z]+'
+# result_all = re.findall(searching, my_text_3)
+
+# for item in result_all:
+#     print(item)
+#     full_name_file.write(item + '\n')
+
+# print(f'Total :  {str(len(result_all))}')
+
+
+# file_path = 'MOCK_DATA.txt'
+# extension_file_path = 'extension.txt'
+# file_reader = open(file_path, mode='r', encoding='Latin-1')
+# extension_file = open(extension_file_path, mode='w', encoding='Latin-1')
+# my_text_4 = file_reader.read()
+
+# searching = r'[\s A-Z]+[\w a-z.]+[\.]+\w+'
+# result_all = re.findall(searching, my_text_4)
+
+# for item in result_all:
+#     print(item)
+#     extension_file.write(item + '\n')
+
+# print(f'Total :  {str(len(result_all))}')
+
